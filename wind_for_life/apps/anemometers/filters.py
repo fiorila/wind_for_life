aa02f0f9b757a30ccdc44a2972e1a9a7f06b05f7
fiorila <fjorilda.gjermizi@gmail.com>
2026-04-29 10:09:52 +0200

    fix add tag filters

--- wind_for_life/apps/anemometers/filters.py
+++ wind_for_life/apps/anemometers/filters.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django_filters import rest_framework as filters
 
 from wind_for_life.apps.anemometers.models import Reading
@@ -14,6 +15,43 @@ class ReadingFilterSet(filters.FilterSet):
         lookup_expr="lte",
         label="Filter readings recorded on or before this date (ISO format)",
     )
+    tags_any = filters.CharFilter(
+        method="filter_tags_any",
+        label="Comma-separated tag list, any of these tags (OR)",
+    )
+    tags_exact = filters.CharFilter(
+        method="filter_tags_exact",
+        label="Comma-separated tag list, exactly of these tags (AND)",
+    )
+
+    def filter_tags_any(self, queryset, name, value):
+        """Compares against an allowed list of tags (OR)"""
+        tags = [tag.strip() for tag in value.split(",") if tag.strip()]
+        return queryset.filter(tags__name__in=tags).distinct()
+
+    def filter_tags_exact(self, queryset, name, value):
+        """
+        Compares against an exact list of tags (AND)
+        """
+        tags = [tag.strip() for tag in value.split(",") if tag.strip()]
+        tag_count = len(tags)
+        if tag_count == 0:
+            return queryset.none()
+
+        queryset = (
+            queryset.annotate(num_tags=Count("tags", distinct=True))
+            .filter(tags__name__in=tags)
+            .filter(num_tags=tag_count)
+            .distinct()
+        )
+
+        readings_with_exact_tags = [
+            reading.pk
+            for reading in queryset.prefetch_related("tags")
+            if set(reading.tags.names()) == set(tags)
+        ]
+
+        return queryset.model.objects.filter(pk__in=readings_with_exact_tags)
 
     class Meta:  # type: ignore  # noqa: PGH003
         model = Reading
